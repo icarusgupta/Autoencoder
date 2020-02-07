9008098b0d6ca389ac6033f416240c9bdf2db422
icarusgupta <60078031+icarusgupta@users.noreply.github.com>
2020-02-07 01:10:54 +0000

    Added download file

--- download_data.py
+++ download_data.py
@@ -1,28 +1,40 @@
-import urllib
+import pandas as pd
+import pandas_datareader as pdr
+from datetime import datetime
+
+from yahoo_finance import Share
 
 
-base_url = "http://ichart.finance.yahoo.com/table.csv?s="
+def pull_historical_data(ticker_symbol, sd, ed):
+    ticker = pdr.get_data_yahoo(symbols=ticker_symbol, start=datetime.strptime(sd, '%Y-%m-%d'),
+                                end=datetime.strptime(ed, '%Y-%m-%d'))
+    ser = ticker['Adj Close']
+    df = pd.DataFrame({ticker_symbol: ser})
+    return df
 
 
-def make_url(ticker_symbol):
-    return base_url + ticker_symbol
+def get_tickers():
+    df = pd.read_csv('data/tickers.txt', sep='\t', header=None, encoding='utf_8')
+    return df[0].tolist()
 
 
-output_path = "C:/path/to/output/directory"
+def create_data_obj(tickers, sd, ed):
+    df = None
+    for ticker in tickers[0:5]:
+        try:
+            df_tmp = pull_historical_data(ticker, sd, ed)
+            if df is None:
+                df = df_tmp
+            else:
+                df = df.merge(df_tmp, how='inner', left_index=True, right_index=True)
+        except:
+            pass
+    return df
 
-
-def make_filename(ticker_symbol, directory="S&P"):
-    return output_path + "/" + directory + "/" + ticker_symbol + ".csv"
-
-
-def pull_historical_data(ticker_symbol, directory="S&P"):
-    try:
-        urllib.urlretrieve(make_url(ticker_symbol), make_filename(ticker_symbol, directory))
-    except urllib.ContentTooShortError as e:
-        outfile = open(make_filename(ticker_symbol, directory), "w")
-        outfile.write(e.content)
-        outfile.close()
 
 if __name__ == "__main__":
 
-    pull_historical_data()
+    tickers = get_tickers()
+    sd = '2015-01-01'
+    ed = '2019-01-01'
+    create_data_obj(tickers, sd, ed)
